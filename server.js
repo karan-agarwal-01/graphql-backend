762fd273d20df0a1eb27e16954d488026489e873
karan-agarwal-01 <karanagarwal858@gmail.com>
2025-12-04 14:15:45 +0530

    new changes 4

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
-const auth = require('./middleware/auth.middleware');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema');
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI
 
 async function start() {
   try {
+    mongoose.set('bufferCommands', false)
+
     await mongoose.connect(MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
     });
 
     console.log('Connected to MongoDB');
+
+    const auth = require('./middleware/auth.middleware');
 
     app.use(auth)
 
